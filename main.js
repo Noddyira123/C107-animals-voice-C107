74ae11dcb08eb76add1ae8f67924e5d021fa7163
Noddyira123 <96566372+Noddyira123@users.noreply.github.com>
2023-02-27 18:39:47 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function startClassification()
+ { navigator.mediaDevices.getUserMedia({ audio: true, video:false});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/NJTCRV9hP/model.json', { probabilityThreshold: 0.7 } ,modelReady); 
+}
+
+function modelReady()
+{
+classifier.classify(gotResults);
+}
+
+var dog = 0;
+var cat = 0;
+
+function gotResults(error,results)
+{
+if(error) {
+    console.error(error);
+}
+}
